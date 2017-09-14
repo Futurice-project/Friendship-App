b9ba54bdc42015f6ae3593b592f15c954e4e1361
pastr <pastor1212@gmail.com>
2017-09-14 17:54:17 +0300

    cleaning of the code and some minor css modification

--- src/components/Person.js
+++ src/components/Person.js
@@ -4,75 +4,58 @@ import styled from 'styled-components/native';
 
 import { FlexRow } from './Layout';
 
-// const ProfileMini = styled.div`
-//                     height: 250px;
-//                     width: 200px;
-//                     border-style: solid;
-//                     border-width: 1px;
-//                     margin-left: 20px;
-//                     `;
+export default class Person extends React.Component {
+  render = () => (
+    <View style={styles.topPart}>
+      <View style={{ flex: 70 }}>
+        <Text style={styles.topText}>{this.props.data.description}</Text>
+      </View>
+
+      <FlexRow style={styles.bottomPart}>
+        <View style={styles.whiteCircle}>
+          <Text style={styles.emoji}>{this.props.data.emoji}</Text>
+        </View>
+        {/* with flex:1 long username don't go exceed the bottom part  */}
+        <View style={{ flex: 1 }}>
+          <Text>{this.props.data.username}</Text>
+        </View>
+      </FlexRow>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-  main: {
+  topPart: {
     height: 300,
     width: 200,
     marginLeft: 20,
     backgroundColor: '#939795',
   },
-  userName: {
+  topText: {
+    color: 'white',
+    marginTop: 5,
+    marginLeft: 10,
+    fontSize: 18,
+    fontFamily: 'Avenir',
+  },
+  bottomPart: {
     width: 200,
     padding: 5,
     backgroundColor: '#E8E9E8',
-    flex: 2,
+    flex: 30,
     alignSelf: 'flex-end',
   },
+  whiteCircle: {
+    width: 50,
+    height: 50,
+    borderRadius: 100 / 2,
+    backgroundColor: 'white',
+    marginRight: 10,
+  },
+  emoji: {
+    backgroundColor: 'transparent',
+    alignSelf: 'center',
+    fontSize: 30,
+    paddingTop: 8,
+  },
 });
-
-export default class Person extends React.Component {
-  render = () => (
-    <View
-      style={
-        styles.main
-        // { height: 250, width: 200, borderStyle: "solid", borderWidth: 1, marginLeft: 20, padding: 5, backgroundColor: this.props.color}
-      }
-    >
-      <View style={{ flex: 4 }}>
-        <Text
-          style={{
-            color: 'white',
-            marginTop: 5,
-            marginLeft: 10,
-            fontSize: 18,
-            fontFamily: 'Avenir',
-          }}
-        >
-          {this.props.data.description}
-        </Text>
-      </View>
-
-      <FlexRow style={styles.userName}>
-        <View
-          style={{
-            width: 50,
-            height: 50,
-            borderRadius: 100 / 2,
-            backgroundColor: 'white',
-          }}
-        >
-          <Text
-            style={{
-              backgroundColor: 'transparent',
-              alignSelf: 'center',
-              fontSize: 30,
-              paddingTop: 8,
-            }}
-          >
-            {this.props.data.emoji}
-          </Text>
-        </View>
-
-        <Text style={{ flex: 5 }}>{this.props.data.username}</Text>
-      </FlexRow>
-    </View>
-  );
-}
