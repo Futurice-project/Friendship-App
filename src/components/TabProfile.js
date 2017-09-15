77ed41f4b7257715281d9124556541fdb9a4c35b
pastr <pastor1212@gmail.com>
2017-09-15 14:33:39 +0300

    creating the component

---
+++ src/components/TabProfile.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Image, View, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+export default class TabProfile extends React.Component {
+  render = () => <View> </View>;
+}
