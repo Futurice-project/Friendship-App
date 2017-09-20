be749e5f48ba47d29627e32e1ad4230871ff1ecb
Mia Lahtinen <beachpanda@hotmail.com>
2017-09-20 14:55:27 +0300

    Infinite scroll with randomuser.me data.

--- src/containers/views/PeopleList.js
+++ src/containers/views/PeopleList.js
@@ -8,7 +8,9 @@ import {
   FlatList,
   ScrollView,
   StyleSheet,
+  ActivityIndicator,
 } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
 
 import { PepperoniLogo, IconButton } from '../../components/Pepperoni';
 import { Title, Description, Bold } from '../../components/Text';
@@ -22,23 +24,71 @@ export class PeopleView extends React.Component {
     title: 'People',
   };
 
-  state = { data: {} };
+  state = {
+    data: {},
+    page: 0,
+    loading: false,
+  };
 
-  keyExtractor = item => item.id;
+  // keyExtractor = item => item.id;
+  keyExtractor = (x, i) => i;
+  // what does (x , i) => i mean. If I didn't change this, it'd say the id for every list item is the same. So this somehow changes the id for each item
 
-  renderItem = ({ item }) => <Person color="#939795" data={item} />;
+  renderItem = ({ item }) => (
+    <ListItem
+      roundAvatar
+      avatar={{ uri: item.picture.thumbnail }}
+      title={`${item.name.first} ${item.name.last}`}
+    />
+  );
+  // <Person color="#939795" data={item} />
 
   componentDidMount() {
-    fetch('http://0.0.0.0:3888/users', {
-      method: 'get',
-      headers: {
-        Authorization:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJmb29AYmFyLmNvbSIsInNjb3BlIjoidXNlciIsImlhdCI6MTUwNDg2NDg0OH0.jk2cvlueBJTWuGB0VMjYnbUApoDua_8FrzogDXzz9iY',
-      },
-    })
-      .then(response => response.json())
-      .then(data => this.setState({ data }));
+    this.fetchData();
   }
+
+  // fetchData = async () => {
+  //  // this.setState({ loading: true });
+  //   const response = await fetch (
+  //     `http://0.0.0.0:3888/users/page/${this.state.page}`, {
+  //         method: 'get',
+  //         headers: {
+  //           Authorization:        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJmb29AYmFyLmNvbSIsInNjb3BlIjoidXNlciIsImlhdCI6MTUwNDg2NDg0OH0.jk2cvlueBJTWuGB0VMjYnbUApoDua_8FrzogDXzz9iY',
+  //         }, });
+  //   const json = await response.json();
+  //   this.setState(state => ({ data: [...state.data, ...json.results], loading: false}));
+  // };
+  // // // // // // // // //
+  fetchData = async () => {
+    // this.setState({ loading: true });
+    const response = await fetch(
+      `https://randomuser.me/api?results=15&seed=hi&page=${this.state.page}`,
+    );
+    const json = await response.json();
+    this.setState(state => ({
+      data: [...state.data, ...json.results],
+      loading: false,
+    }));
+  };
+
+  // componentDidMount() {
+  //   fetch('http://0.0.0.0:3888/users/page/4', {
+  //     method: 'get',
+  //     headers: {
+  //       Authorization:
+  //         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJmb29AYmFyLmNvbSIsInNjb3BlIjoidXNlciIsImlhdCI6MTUwNDg2NDg0OH0.jk2cvlueBJTWuGB0VMjYnbUApoDua_8FrzogDXzz9iY',
+  //     },
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ data }));
+  // }
+
+  handleEnd = () => {
+    this.setState(
+      state => ({ page: this.state.page + 1 }),
+      () => this.fetchData(),
+    );
+  };
 
   render = () => (
     <ViewContainer>
@@ -48,7 +98,10 @@ export class PeopleView extends React.Component {
           data={this.state.data}
           keyExtractor={this.keyExtractor}
           renderItem={this.renderItem}
-          horizontal={true}
+          onEndReached={this.handleEnd}
+          onEndReachedThreshold={0}
+          // ListFooterComponent= {() => <ActivityIndicator animating size= 'small'/>}
+          horizontal
         />
       </Centered>
     </ViewContainer>
