be87e2237181a66e630226f3893ba2369b64ab4d
jyrienstra <joukerienstra@hotmail.com>
2017-09-29 15:30:47 +0300

    header styling added to personality view

--- src/containers/views/PersonalityView.js
+++ src/containers/views/PersonalityView.js
@@ -1,29 +1,61 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import rest from '../../utils/rest';
+import styled from 'styled-components/native';
 
 import { Description, Bold } from '../../components/Text';
-import {
-  PersonalityContainer,
-  Padding,
-  Centered,
-} from '../../components/Layout';
-import TextInput from '../../components/TextInput';
-import RoundTab from '../../components/RoundTab';
+import { SignUpWrapper, Padding, Centered } from '../../components/Layout';
+import Label from '../../components/Label';
 import { TouchableOpacity, Text, KeyboardAvoidingView } from 'react-native';
 
 class PersonalityView extends React.Component {
   static navigationOptions = {
     title: 'Personality',
+    header: () => null,
   };
 
   render() {
     return (
-      <PersonalityContainer>
-        <Text>aaa</Text>
-      </PersonalityContainer>
+      <SignUpWrapper>
+        <Padding>
+          <Header>
+            <HeaderRectangle>
+              <Text style={styles.title}>Things that describe me</Text>
+              <Text style={styles.subTitle}>
+                These will be shown in your profile
+              </Text>
+            </HeaderRectangle>
+          </Header>
+        </Padding>
+      </SignUpWrapper>
     );
   }
 }
+
+const Header = styled.View`
+  display: flex;
+  height: 200;
+  width: 100%;
+`;
+const HeaderRectangle = styled.View`
+  background-color: red;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const styles = {
+  title: {
+    fontFamily: 'NunitoSans-Bold',
+    fontSize: 40,
+    color: '#2d4359',
+  },
+  subTitle: {
+    marginTop: 5,
+    fontFamily: 'NunitoSans-Regular',
+    fontSize: 15,
+    color: '#2d4359',
+  },
+};
 
 export default connect(undefined)(PersonalityView);
