50153ea4b17e18df0493d9e320ef06f795906cc4
Jokester92 <joanikki@welho.com>
2017-09-29 16:05:05 +0300

    Age edited to early's, mid's, late's

--- src/containers/views/ProfileUser.js
+++ src/containers/views/ProfileUser.js
@@ -48,6 +48,25 @@ class ProfileUser extends React.Component {
     }
   }
 
+    const early = [0, 1, 2, 3];
+    const mid = [4, 5, 6];
+    const late = [7, 8, 9];
+    let ageName = '';
+    const lastDigit = age.toString().substr(age.toString().length - 1);
+    if (early.indexOf(parseInt(lastDigit)) > -1) {
+      ageName = 'early ' + (age - parseInt(lastDigit)) + "'s";
+    } else if (mid.indexOf(parseInt(lastDigit)) > -1) {
+      ageName = 'mid ' + (age - parseInt(lastDigit)) + "'s";
+    } else if (late.indexOf(parseInt(lastDigit)) > -1) {
+      ageName = 'late ' + (age - parseInt(lastDigit)) + "'s";
+    } else {
+      ageName = "It's a mystery";
+    }
+
+    this.setState({ age: ageName });
+  };
+
+  setAge = () => {};
   componentDidMount() {
     const personId = this.props.navigation.state.params.personId;
     this.props.refreshUser(personId);
