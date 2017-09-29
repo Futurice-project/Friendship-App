c8c1784cec2ff03e801f15182e0dc73db7074746
robsel118 <robertleeseligmann@gmail.com>
2017-09-29 15:06:57 +0300

    Toggle Done

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -10,6 +10,24 @@ export const ToggleSwitch = styled.View`
   left: ${props => (props.value ? 5 : 72)};
 `;
 
+export const ToggleText = styled.Text`
+  height: 50;
+
+  font-family: 'NunitoSans-Regular';
+  font-size: 13;
+  letter-spacing: 1.59;
+  text-align: ${props => (props.right ? 'right' : 'left')}};
+  padding: 5px;
+  color: #fff;
+  text-align-vertical: center;
+`;
+export const ContainerView = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+`;
+
 export const ToggleView = styled.View`
   background-color: #ffffff;
   width: 97;
@@ -17,19 +35,39 @@ export const ToggleView = styled.View`
   border-width: 0.5;
   border-color: #e1e1e1;
   border-radius: 40;
+  align-items: center;
   flex-direction: row;
+`;
+
+export const TextWrapper = styled.View`
+  flex-wrap: nowrap;
+  flex: 1;
 `;
 /**
  * Styled Toggle component
  * @param {boolean} value - Indicating the value of the switch
   * @param {function} onPress - Sets the function -->change the Toggle's state
+	* @param {boolean} right - specify for the left text to align on the left side
+	* @param {string} rightText - Sets the text of the right side of the switch
+	* @param {string} leftText - Sets the text of the left side of the switch
  */
 export default class Toggle extends React.Component {
   render = () => (
-    <TouchableWithoutFeedback onPress={this.props.onPress}>
-      <ToggleView>
-        <ToggleSwitch value={this.props.value} />
-      </ToggleView>
-    </TouchableWithoutFeedback>
+    <ContainerView>
+      <TextWrapper>
+        <ToggleText right>{this.props.leftText}</ToggleText>
+      </TextWrapper>
+      <TouchableWithoutFeedback
+        style={{ flex: 1 }}
+        onPress={this.props.onPress}
+      >
+        <ToggleView>
+          <ToggleSwitch value={this.props.value} />
+        </ToggleView>
+      </TouchableWithoutFeedback>
+      <TextWrapper>
+        <ToggleText>{this.props.rightText}</ToggleText>
+      </TextWrapper>
+    </ContainerView>
   );
 }
