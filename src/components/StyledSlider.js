fce8fc86fd715bccfbba837096f6a3d2b1687070
jyrienstra <joukerienstra@hotmail.com>
2017-10-11 13:46:26 +0300

    Updated slider with text underneath

--- src/components/StyledSlider.js
+++ src/components/StyledSlider.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import Slider from 'react-native-slider';
+import styled from 'styled-components/native';
+import { Text } from 'react-native';
 
 export default class StyledSlider extends React.Component {
   render = () => (
-    <Slider
-      trackStyle={style.track}
-      thumbStyle={style.thumb}
-      minimumTrackTintColor="#e8e9e8"
-    />
+    <Wrapper>
+      <Slider
+        trackStyle={style.track}
+        thumbStyle={style.thumb}
+        minimumTrackTintColor="#e8e9e8"
+      />
+      <TextWrapper>
+        <Text style={style.sliderText}>Ei yhtään</Text>
+        <Text style={style.sliderText}>En tiedä</Text>
+        <Text style={style.sliderText}>Paljon</Text>
+      </TextWrapper>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.View`display: flex;`;
+const TextWrapper = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-left: 5;
+  margin-right: 5;
+`;
 
 const style = {
   track: {
@@ -24,5 +42,10 @@ const style = {
     borderColor: '#89dd93',
     borderWidth: 14,
     borderRadius: 15,
+  },
+  sliderText: {
+    fontFamily: 'NunitoSans-Light',
+    fontSize: 13,
+    color: '#2d4359',
   },
 };
