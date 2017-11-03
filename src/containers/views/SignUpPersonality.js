47d3ea7dfde09c5425acf44e84c8b61337993bc4
jyrienstra <joukerienstra@hotmail.com>
2017-11-03 21:57:35 +0200

    create new personalityview, edit navigation from signuplocation->personality view

---
+++ src/containers/views/SignUpPersonality.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import rest from '../../utils/rest';
+import Button from '../../components/Button';
+import { Description, Bold } from '../../components/Text';
+import { ViewContainer, Padding, Centered } from '../../components/Layout';
+import TextInput from '../../components/TextInput';
+import RoundTab from '../../components/RoundTab';
+import styled from 'styled-components/native';
+import { NavigationActions } from 'react-navigation';
+
+import {
+  TouchableOpacity,
+  Text,
+  KeyboardAvoidingView,
+  Keyboard,
+  View,
+  Platform,
+  Dimensions,
+} from 'react-native';
+
+class SignUpPersonality extends React.Component {
+  render() {
+    return (
+      <ViewContainer>
+        <Text>aa</Text>
+      </ViewContainer>
+    );
+  }
+}
+
+const HeaderWrapper = styled.View`
+  margin-top: 20;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const styles = {
+  headerText: {
+    fontFamily: 'NunitoSans-SemiBold',
+    fontSize: 20,
+    color: '#ff8a65',
+  },
+  buttonTextStyle: {
+    width: 230,
+    height: 27,
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontFamily: 'NunitoSans-Regular',
+    textAlign: 'center',
+    color: 'white',
+  },
+  statusTextStyle: {
+    fontFamily: 'NunitoSans-Regular',
+    width: '100%',
+    height: 20,
+    fontSize: 15,
+    textAlign: 'center',
+    color: '#f673f7',
+    marginBottom: 10,
+  },
+  textStyle: {
+    fontFamily: 'NunitoSans-Regular',
+    width: '100%',
+    height: 20,
+    fontSize: 15,
+    textAlign: 'center',
+    color: '#f9f7f6',
+    marginBottom: 10,
+  },
+};
+
+export default SignUpPersonality;
