062428a6a41cc8fb10cd568896db8a52211bc62e
Lisa Petrova <elizavetap.v@gmail.com>
2017-11-15 19:40:48 +0200

    edited gender in profile view

--- src/utils/rest.js
+++ src/utils/rest.js
@@ -27,6 +27,16 @@ if (process.env.NODE_ENV === 'development') {
   apiRoot = 'https://my-app.herokuapp.com';
 }
 
+authTransformer = (data = {}) => {
+  if (data.token) {
+    return {
+      ...data,
+      decoded: jwtDecode(data.token),
+    };
+  }
+  return data;
+};
+
 const rest = reduxApi({
   personalities: {
     url: `${apiRoot}/personalities`,
@@ -38,6 +48,10 @@ const rest = reduxApi({
   },
   users: {
     url: `${apiRoot}/users`,
+    crud: true,
+  },
+  usersByPage: {
+    url: `${apiRoot}/users/page/:number`,
     crud: true,
   },
   usersSearch: {
@@ -54,6 +68,11 @@ const rest = reduxApi({
     transformer: transformers.array,
     crud: true,
   },
+  userGenders: {
+    url: `${apiRoot}/user_gender/:userId`,
+    transformer: transformers.array,
+    crud: true,
+  },
   currentUser: {
     url: `${apiRoot}/users/:userId`,
     crud: true,
@@ -63,22 +82,64 @@ const rest = reduxApi({
     transformer: transformers.array,
     crud: true,
   },
-  userGenders: {
+  currentUserGenders: {
     url: `${apiRoot}/user_gender/:userId`,
+    transformer: transformers.array,
     crud: true,
   },
-
+  reports: {
+    url: `${apiRoot}/reports`,
+    crud: true,
+  },
+  chatRooms: {
+    url: `${apiRoot}/chatrooms`,
+    crud: true,
+  },
+  createChatRoom: {
+    url: `${apiRoot}/chatrooms`,
+    crud: true,
+    options: {
+      method: 'POST',
+    },
+  },
+  chatRoomsWithUserId: {
+    url: `${apiRoot}/chatrooms/userid/:id`,
+    crud: true,
+  },
+  chatRoomMessages: {
+    url: `${apiRoot}/chatrooms/:id`,
+    crud: true,
+  },
+  sendMessage: {
+    url: `${apiRoot}/chatrooms/:id`,
+    crud: true,
+    options: {
+      method: 'POST',
+    },
+    postfetch: [
+      function({ dispatch, actions, data }) {
+        const id = data.chatroom_id;
+        dispatch(actions.chatRoomMessages({ id }));
+      },
+    ],
+  },
+  createUserPersonality: {
+    url: `${apiRoot}/user_personality`,
+    options: { method: 'POST' },
+  },
+  createUserPersonalities: {
+    url: `${apiRoot}/user_personalities`,
+    options: { method: 'POST' },
+  },
+  register: {
+    url: `${apiRoot}/users`,
+    transformer: authTransformer,
+    reducerName: 'auth',
+    options: { method: 'POST' },
+  },
   auth: {
     url: `${apiRoot}/users/authenticate`,
-    transformer: (data = {}) => {
-      if (data.token) {
-        return {
-          ...data,
-          decoded: jwtDecode(data.token),
-        };
-      }
-      return data;
-    },
+    transformer: authTransformer,
     options: {
       method: 'POST',
     },
@@ -106,13 +167,8 @@ const rest = reduxApi({
   .use('fetch', adapterFetch(fetch))
   .use('responseHandler', (err, data) => {
     if (err) {
-      console.log('Error', err);
+      throw err;
     }
-    if (data) {
-      // console.log('Success', data);
-      return data;
-    }
-    throw err;
   });
 
 export default rest;
