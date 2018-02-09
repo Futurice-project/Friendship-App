0c2752a84c099346a9ec03e2fee3f5f05048a468
Kirill Polyanin <k.polianin@gmail.com>
2018-02-09 23:32:42 +0200

    Added authorization check in events view

--- src/containers/views/EventsView.js
+++ src/containers/views/EventsView.js
@@ -6,6 +6,15 @@ import { IconImage } from '../../components/Layout/Layout';
 import EventsHeader from '../../components/Events/EventsHeader';
 import EventsList from '../../components/Events/EventsList';
 
+const mapStateToProps = state => ({
+  events: state.events,
+  auth: state.auth,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchEvents: () => dispatch(rest.actions.events.get()),
+});
+
 class EventsView extends Component {
   static navigationOptions = {
     title: 'Events',
@@ -24,27 +33,34 @@ class EventsView extends Component {
     this.props.fetchEvents();
   };
 
-  render = () => {
+  renderContent = () => {
     const { events } = this.props;
 
-    if (events.loading) {
-      return <ActivityIndicator />;
+    if (!events.loading) {
+      return <EventsList events={events} />;
     }
+
+    return <ActivityIndicator />;
+  };
+
+  render = () => {
+    if (!this.props.auth.data.decoded) {
+      return (
+        <View style={{ backgroundColor: '#e8e9e8' }}>
+          <Text style={{ color: 'red', fontSize: 15 }}>
+            You need to sign in!
+          </Text>
+        </View>
+      );
+    }
+
     return (
       <View style={{ flex: 1 }}>
         <EventsHeader headerText="Events" />
-        <EventsList events={events} />
+        {this.renderContent()}
       </View>
     );
   };
 }
-
-const mapStateToProps = state => ({
-  events: state.events,
-});
-
-const mapDispatchToProps = dispatch => ({
-  fetchEvents: () => dispatch(rest.actions.events.get()),
-});
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventsView);
