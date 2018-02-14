a066ef62ee0c0d19be47aac8460f2dfcdd09963b
Kirill Polyanin <k.polianin@gmail.com>
2018-02-14 14:25:54 +0200

    Set up back navigation in Event's detail page and fixed bug image not rendering

--- src/containers/views/EventDetailView.js
+++ src/containers/views/EventDetailView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Text } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
+import { NavigationActions } from 'react-navigation';
 
 import rest from '../../utils/rest';
 import {
@@ -12,6 +13,7 @@ import { Description } from '../../components/Layout/TextLayout';
 import EventTopPart from '../../components/Events/EventTopPart';
 
 const mapStateToProps = state => ({
+  auth: state.auth,
   eventDetails: state.eventDetails,
 });
 
@@ -20,35 +22,68 @@ const mapDispatchToProps = dispatch => ({
 });
 
 class EventDetailView extends Component {
+  state = {
+    loaded: false,
+  };
+
   componentDidMount = () => {
     const { eventId } = this.props.navigation.state.params;
     this.props.fetchEvent(eventId);
   };
 
+  componentWillReceiveProps(nextProps) {
+    // render the event details when we have the data.
+    if (!nextProps.eventDetails.loading) {
+      this.setState({
+        loaded: true,
+      });
+    }
+  }
+
+  navigateBack = () => {
+    const backAction = NavigationActions.back();
+    this.props.navigation.dispatch(backAction);
+  };
+
   render() {
-    console.log(this.props.eventDetails);
-    const { title, description, location } = this.props.eventDetails.data;
+    if (!this.props.auth.data.decoded) {
+      return (
+        <View style={{ marginTop: 30 }}>
+          <Text style={{ alignSelf: 'center' }}>You need to sign in!</Text>
+        </View>
+      );
+    }
+    if (!this.state.loaded) {
+      return <ActivityIndicator />;
+    } else {
+      console.log(this.props.eventDetails.data);
+      const { title, description, location } = this.props.eventDetails.data;
 
-    // if there is no picture for the user we use a default image
-    const eventImage = this.props.eventDetails.data.eventImage
-      ? {
-          uri:
-            'data:image/png;base64,' + this.props.eventDetails.data.eventImage,
-        }
-      : require('../../../assets/img/placeholder/grone.jpg');
+      // if there is no picture for the user we use a default image
+      const srcImage = this.props.eventDetails.data.eventImage
+        ? {
+            uri:
+              'data:image/png;base64,' +
+              this.props.eventDetails.data.eventImage,
+          }
+        : require('../../../assets/img/placeholder/grone.jpg');
 
-    return (
-      <EventContainer>
-        <EventTopPart
-          eventTitle={title}
-          location={location}
-          srcImage={eventImage}
-        />
-        <DescriptionWrapper>
-          <Description>{description}</Description>
-        </DescriptionWrapper>
-      </EventContainer>
-    );
+      console.log(srcImage);
+
+      return (
+        <EventContainer>
+          <EventTopPart
+            eventTitle={title}
+            location={location}
+            srcImage={srcImage}
+            navigateBack={this.navigateBack}
+          />
+          <DescriptionWrapper>
+            <Description>{description}</Description>
+          </DescriptionWrapper>
+        </EventContainer>
+      );
+    }
   }
 }
 
