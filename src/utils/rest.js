5ca3c98cda76efe53e013ce4ecf96786fed10e3b
Minh Hoang <minh.hn2016@gmail.com>
2018-04-17 11:22:08 +0700

    colored send & receive card bubbles

--- src/utils/rest.js
+++ src/utils/rest.js
@@ -29,7 +29,9 @@ let apiRoot;
  * */
 if (process.env.NODE_ENV === 'development') {
   apiRoot =
-    Platform.OS === 'ios' ? 'http://localhost:3888' : 'http://localhost:3888';
+    Platform.OS === 'ios'
+      ? 'http://192.168.1.73:3888'
+      : 'http://localhost:3888';
 } else {
   apiRoot = 'https://friendshipapp-backend.herokuapp.com';
 }
