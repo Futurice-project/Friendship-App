75e2de79c9a2705cb57badca0ccbda58807db126
chengjo0 <john.cheng@futurice.com>
2018-04-18 01:55:56 +0300

    Validation for locations added

--- src/containers/views/SignUp/SignUpLocation.js
+++ src/containers/views/SignUp/SignUpLocation.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import { LOCATION } from '../../../components/SignUp/Constants';
-import validate from '../../../components/SignUp/validate';
+import {
+  checkErrorMessage,
+  renderErrorMessage,
+  validateLocations,
+} from '../../../components/SignUp/validate';
 import { Field, reduxForm } from 'redux-form';
 import LocationList from '../../../components/SignUp/LocationList';
 import { View } from 'react-native';
 import SignupHeader from '../../../components/SignUp/SignupHeader';
 import {
   Container,
+  FieldContainer,
+  FieldWrapper,
   LabelText,
   Part,
 } from '../../../components/Layout/SignupLayout';
 import Footer from '../../../components/SignUp/Footer';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => ({
+  signup: state.form.signup,
+});
+
+const renderLocationField = submittedErrors => {
+  const err = checkErrorMessage(submittedErrors, 'LOCATIONS');
+  return (
+    <Container style={{ flex: 1, backgroundColor: '#efebe9' }}>
+      <Part style={{ flex: 2 }}>
+        <LabelText style={{ marginTop: 21, marginLeft: 30, marginRight: 30 }}>
+          With your location, we will find the happenings, groups and people
+          closest to you.
+        </LabelText>
+      </Part>
+      <Part style={{ flex: 8, marginTop: 45 }}>
+        <Field name={'locations'} component={LocationList} />
+        <FieldContainer>{err ? renderErrorMessage(err) : null}</FieldContainer>
+      </Part>
+      <Footer style={{ flex: 1 }} textColor={'blue'} />
+    </Container>
+  );
+};
 
 class SignUpLocation extends React.Component {
   state = {
@@ -23,31 +53,22 @@ class SignUpLocation extends React.Component {
         style={{ display: 'flex', height: '100%', flexDirection: 'column' }}
       >
         <SignupHeader processStage={LOCATION} headerTitle={'HEY !'} />
-        <Container style={{ flex: 1, backgroundColor: '#efebe9' }}>
-          <Part style={{ flex: 2 }}>
-            <LabelText
-              style={{ marginTop: 21, marginLeft: 30, marginRight: 30 }}
-            >
-              With your location, we will find the happenings, groups and people
-              closest to you.
-            </LabelText>
-          </Part>
-          <Part style={{ flex: 8, marginTop: 45 }}>
-            <Field name={'locations'} component={LocationList} />
-          </Part>
-          <Footer style={{ flex: 1 }} textColor={'blue'} />
-        </Container>
+        {renderLocationField(
+          this.props.signup ? this.props.signup.submitErrors : null,
+        )}
       </View>
     );
   }
 }
 
-export default reduxForm({
-  form: 'signup',
-  destroyOnUnmount: false,
-  forceUnregisterOnUnmount: true,
-  onSubmit: validate,
-  onSubmitSuccess: (result, dispatch, props) => {
-    dispatch(props.onSubmitSucceeded);
-  },
-})(SignUpLocation);
+export default connect(mapStateToProps, null)(
+  reduxForm({
+    form: 'signup',
+    destroyOnUnmount: false,
+    forceUnregisterOnUnmount: true,
+    onSubmit: validateLocations,
+    onSubmitSuccess: (result, dispatch, props) => {
+      dispatch(props.onSubmitSucceeded);
+    },
+  })(SignUpLocation),
+);
