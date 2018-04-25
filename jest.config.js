a0137098f42a20b213b2299bdec648cfc3220f4e
Emmanuel Pastor <emmanuel.pastor@futurice.com>
2018-04-25 12:24:58 +0300

    Add tests

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,14 @@
 const config = {
   preset: 'jest-expo',
-  haste: {
-    defaultPlatform: 'android',
-    platforms: ['android', 'ios', 'native'],
-    providesModuleNodeModules: ['react-native'],
-  },
   transformIgnorePatterns: ['node_modules/(?!react-native|react-navigation)'],
   setupFiles: ['./enzyme.config.js'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.js'],
   coverageReporters: ['text-summary', 'html'],
+  moduleNameMapper: {
+    '^image![a-zA-Z0-9$_-]+$': 'GlobalImageStub',
+    '^[@./a-zA-Z0-9$_-]+\\.(png|gif)$': 'RelativeImageStub',
+  },
 };
 
 module.exports = config;
---
+++ src/components/Button.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { ButtonWrapper } from './Button';
+
+describe('ButtonWrapper', () => {
+  test('renders correctly', () => {
+    const wrapper = shallow(<ButtonWrapper />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('props change the design correctly', () => {
+    // we use shallow rendering to test our components
+    // http://airbnb.io/enzyme/docs/api/shallow.html
+    const button = shallow(<ButtonWrapper />);
+    const buttonBorder = shallow(<ButtonWrapper border />);
+    const buttonPrimary = shallow(<ButtonWrapper primary />);
+    const buttonPrimaryLight = shallow(<ButtonWrapper primary color="light" />);
+
+    // see: http://airbnb.io/enzyme/docs/api/ShallowWrapper/debug.html
+    // console.log(button.debug());
+    console.log(buttonPrimary.debug());
+
+    // you can see in the console that the backgroundColor is different for each of them
+    // console.log('button props:', button.props());
+    console.log('buttonPrimary props:', buttonPrimary.props());
+    console.log('buttonPrimaryLight props', buttonPrimaryLight.props());
+    // console.log('buttonBorder props', buttonBorder.props());
+
+    // console.log('button state: ', button.state());
+    // console.log('buttonPrimary state:', buttonPrimary.state());
+    // console.log('buttonPrimaryLight state:', buttonPrimaryLight.state());
+
+    // console.log(button.props().style[0].backgroundColor);
+    console.log(buttonPrimary.props().style[0].backgroundColor);
+    console.log(buttonPrimaryLight.props().style[0].backgroundColor);
+
+    expect(button.props().style[0].backgroundColor).toBe(undefined);
+    expect(buttonPrimary.props().style[0].backgroundColor).toBe(
+      'rgba(94, 104, 109, 1)',
+    );
+    expect(buttonPrimaryLight.props().style[0].backgroundColor).toBe(
+      'rgba(255, 255, 255, 1)',
+    );
+
+    expect(buttonBorder.props().style[0].borderWidth).toBe(2);
+  });
+});
