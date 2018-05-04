e7b3637905aed896863ebfb72f3000a21f13cba5
Lam Ngo <lam.ngo.qu@gmail.com>
2018-05-04 13:35:07 +0300

    Initial testing. ButtonTextInput test works fine. ChatInbox test has issue with react-native-vector-icons transpiling

--- src/containers/views/Report/Report.js
+++ src/containers/views/Report/Report.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { ScrollView, View, Text, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+// import Icon from 'react-native-vector-icons/Ionicons';
 import {
   FeedbackListItem,
   reportFields,
