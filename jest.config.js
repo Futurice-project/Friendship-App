775f0cb0ebabcfef8b01df20a6fb1a0263e416aa
Kai Pörtfors <kai.portfors@gmail.com>
2018-05-06 20:20:09 +0300

    updated jest file

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,15 @@
 const config = {
   preset: 'jest-expo',
-  //  transformIgnorePatterns: ['node_modules/(?!react-native|react-navigation)'],
+  transformIgnorePatterns: [
+    'node_modules/(?!react-native|react-navigation|@expo|expo)',
+  ],
   setupFiles: ['./enzyme.config.js'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.js'],
   coverageReporters: ['text-summary', 'html'],
   moduleNameMapper: {
     '^image![a-zA-Z0-9$_-]+$': 'GlobalImageStub',
-    '^[@./a-zA-Z0-9$_-]+\\.(png|gif)$': 'RelativeImageStub',
+    '^[@./a-zA-Z0-9$_-]+\\.(bmp|gif|jpg|jpeg|png|svg)$': 'RelativeImageStub',
   },
 };
-
 module.exports = config;
