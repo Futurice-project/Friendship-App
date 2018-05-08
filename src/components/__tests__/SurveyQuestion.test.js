22b42b0402c798947d31a1c93daf1d3b6b33fd51
Abdelkarim Essalim <abdelkarim.essalim@myy.haaga-helia.fi>
2018-05-08 11:40:58 +0300

    Finally got the other SurveyQuestion tests to work

--- src/components/__tests__/SurveyQuestion.test.js
+++ src/components/__tests__/SurveyQuestion.test.js
@@ -4,8 +4,30 @@ import { shallow } from 'enzyme';
 import SurveyQuestion from '../Feedback/SurveyQuestion';
 
 describe('SurveyQuestion', () => {
+  const subtitle = 'Tell us anything you like!';
+  const onChange = jest.fn();
+
   test('SurveyQuestion renders correctly', () => {
     const wrapper = shallow(<SurveyQuestion />);
     expect(wrapper).toMatchSnapshot();
+  });
+  test('SurveyQuestion receives props correctly', () => {
+    const wrapper = shallow(
+      <SurveyQuestion
+        title={'Hello'}
+        subtitle={subtitle}
+        onChange={onChange}
+        value={'hi'}
+      />,
+    );
+    const surveyQuestion = wrapper.find('TextInput');
+    console.log(surveyQuestion.props());
+    expect(surveyQuestion.props().placeholder).toEqual(
+      'Tell us anything you like!',
+    );
+
+    expect(surveyQuestion.props().value).toEqual('hi');
+
+    expect(surveyQuestion.props().onChangeText()).toMatchSnapshot();
   });
 });
