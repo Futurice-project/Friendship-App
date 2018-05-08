eaeb1c86db669d66842c51b415a50577a8c6bdfc
Abdelkarim Essalim <abdelkarim.essalim@myy.haaga-helia.fi>
2018-05-08 10:30:54 +0300

    Deleted useless tests that did nothing in SurveyQuestion, only snapshot now

--- src/components/__tests__/SurveyQuestion.test.js
+++ src/components/__tests__/SurveyQuestion.test.js
@@ -4,38 +4,8 @@ import { shallow } from 'enzyme';
 import SurveyQuestion from '../Feedback/SurveyQuestion';
 
 describe('SurveyQuestion', () => {
-  const subtitle = 'Tell us anything you like!';
-  const onChange = () => {
-    findFriendEasy => this.setState({ findFriendEasy });
-  };
-  const value = 'Change how?';
-  const title = 'Hello';
-  const data = {
-    title: 'Hello',
-    subtitle: 'Tell us anything you like!',
-    onChange: findFriendEasy => this.setState({ findFriendEasy }),
-    value: 'Change how?',
-  };
-
   test('SurveyQuestion renders correctly', () => {
     const wrapper = shallow(<SurveyQuestion />);
     expect(wrapper).toMatchSnapshot();
-  });
-  test('SurveyQuestion receives props correctly', () => {
-    const surveyTitle = shallow(<SurveyQuestion title={title} />);
-    expect(
-      surveyTitle
-        .find('Text')
-        .contains('Hello')
-        .toBe(true),
-    );
-    const surveyQuestion = shallow(<SurveyQuestion data={data} />);
-    const surveySubtitle = shallow(<SurveyQuestion subtitle={subtitle} />);
-    const surveyValue = shallow(<SurveyQuestion value={value} />);
-    console.log(surveyQuestion.instance());
-    expect(surveyQuestion.instance().props().data.subtitle).toEqual(
-      'Tell us anything you like!',
-    );
-    expect(surveyQuestion.instance().props().data.value).toEqual('Change how?');
   });
 });
