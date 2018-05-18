7e6ced02949db472c0130f1eeb615c28b4a1d5f3
John Cheng <chengjo0@users.noreply.github.com>
2018-05-18 12:00:48 +0300

    Chat inbox empty (#89)
    * Empty chatbox message added need styling
    
    * Empty chatbox message updated with styling and redirect to people view

--- src/containers/views/Chat/ChatInbox.js
+++ src/containers/views/Chat/ChatInbox.js
@@ -7,6 +7,7 @@ import RoundTab from '../../../components/RoundTab';
 import InboxCard from '../../../components/InboxCard';
 import SuggestionList from '../../../components/SuggestionList';
 import Report from '../Report/Report';
+import { NavigationActions } from 'react-navigation';
 
 const mapStateToProps = state => ({
   currentUserId: state.auth.data.decoded ? state.auth.data.decoded.id : null,
@@ -18,6 +19,8 @@ const mapDispatchToProps = dispatch => ({
   chatRoomsWithUserId: id => {
     dispatch(rest.actions.chatRoomsWithUserId({ id }));
   },
+  goToPeopleView: () =>
+    dispatch(NavigationActions.navigate({ routeName: 'People' })),
 });
 
 export class ChatInbox extends React.Component {
@@ -55,12 +58,31 @@ export class ChatInbox extends React.Component {
     return (
       <View style={{ flex: 1, paddingTop: 20 }}>
         <RoundTab tint="#ffffff" title="CHATS" fontSize="12" />
-        <FlatList
-          data={sortedChatrooms}
-          keyExtractor={this.keyExtractor}
-          renderItem={this.renderItem}
-          style={{ flex: 1, backgroundColor: 'white', minHeight: 300 }}
-        />
+        {sortedChatrooms.length > 0 ? (
+          <FlatList
+            data={sortedChatrooms}
+            keyExtractor={this.keyExtractor}
+            renderItem={this.renderItem}
+            style={{ flex: 1, backgroundColor: 'white', minHeight: 300 }}
+          />
+        ) : (
+          <View style={{ flex: 1, backgroundColor: '#fff', padding: 20 }}>
+            <Text style={{ fontWeight: 'bold', paddingBottom: 20 }}>
+              Get busy chatting!
+            </Text>
+            <Text style={{ paddingBottom: 20 }}>
+              You have no active chats yet. To start a new chat, first find a
+              person that seems interesting to you. Open their profile, see what
+              you have in common and take it from there.
+            </Text>
+            <Text
+              style={{ textDecorationLine: 'underline', color: '#ff8a65' }}
+              onPress={() => this.props.goToPeopleView()}
+            >
+              Browse profiles
+            </Text>
+          </View>
+        )}
       </View>
     );
   }
